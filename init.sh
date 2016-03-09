fc894a5db60eff93bd1faf3080be88e11af51103
sasha <sasha@localhost.localdomain>
2016-03-09 22:17:26 +0300

    nginx conf

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #sudo apt-get install nginx
 sudo rm /etc/nginx/sites-enabled/default.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s web/etc/nginx.conf  /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
