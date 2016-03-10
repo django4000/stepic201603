a2e69e0c32be8c991eb4b3ca2e2ca89ecff49613
sasha <sasha@localhost.localdomain>
2016-03-10 14:55:33 +0300

    http://lucumr.pocoo.org/2007/5/21/getting-started-with-wsgi/

--- web/hello.py
+++ web/hello.py
@@ -1,3 +1,5 @@
-def wsgi_application(env,response)
-        response('200 OK', [])
-	return [env.wsgi.input]
+from cgi import parse_qs
+def wsgi_application(env,start_response)
+   pars=parse_qs(env.get('QUERY_STRING'))
+   start_response('200 OK', [('Content-Type':'text/html')])
+   return [pars]
