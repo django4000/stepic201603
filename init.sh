a53936329b46aa7b246118403153143578842986
sasha <sasha@localhost.localdomain>
2016-03-13 17:54:45 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #sudo apt-get install nginx
 pip install gunicorn
-sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo rm /etc/nginx/sites-enabled/default.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default.conf
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
