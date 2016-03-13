0139f841971220e853f108d177af5ae753d63208
sasha <sasha@localhost.localdomain>
2016-03-13 18:45:32 +0300

    fix

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -3,7 +3,7 @@ CONFIG = {
     #'working_dir': '/home/box/web/',
     # 'python': '/usr/bin/python',
     'args': (
-        '--chdir=/home/box/web'
+        '--chdir=/home/box/web',
         '--bind=0.0.0.0:8000',
         '--workers=16',
         '--timeout=60',
