5b673f8279f88815606c3d9ca25bf4180fcd963c
sasha <sasha@localhost.localdomain>
2016-03-13 18:43:51 +0300

    fix

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -1,11 +1,12 @@
 CONFIG = {
     # 'mode': 'wsgi',
-    'working_dir': '/home/box/web/',
+    #'working_dir': '/home/box/web/',
     # 'python': '/usr/bin/python',
     'args': (
+        '--chdir=/home/box/web'
         '--bind=0.0.0.0:8000',
         '--workers=16',
         '--timeout=60',
-        'hello.hello',
-    ),                                                                                                                                                                                                         
+        'hello',
+    ),
 } 
