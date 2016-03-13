c7b50885b4a5d2f4cb9e01307eee920040237f6d
sasha <sasha@localhost.localdomain>
2016-03-13 16:20:52 +0300

    fix

--- web/hello.py
+++ web/hello.py
@@ -1,6 +1,6 @@
-from cgi import parse_qs
+
 def hello (env,start_response):
-   #pars=parse_qs(env.get('QUERY_STRING'))
+   
    pars=env.get('QUERY_STRING').split("&")
    start_response('200 OK', [('Content-Type','text/html')])
    return [pars]
