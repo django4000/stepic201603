74cc9b772966db08933f5a6dbd6f4369e84a85b3
sasha <sasha@localhost.localdomain>
2016-03-13 19:06:19 +0300

    fix

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -1,12 +1,12 @@
 CONFIG = {
     # 'mode': 'wsgi',
-    'working_dir': '/home/box/web/',
+    'working_dir': '/home/box/stepic201603/web/',
     # 'python': '/usr/bin/python',
     'args': (
         
         '--bind=0.0.0.0:8080',
         '--workers=16',
         '--timeout=60',
-        'hello',
+        'hello:hello',
     ),
 } 
