e469e05253714dbe0d0f16ce62dfde76d218c4fc
sasha <sasha@localhost.localdomain>
2016-03-13 18:47:27 +0300

    gunicorn config

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -1,9 +1,9 @@
 CONFIG = {
     # 'mode': 'wsgi',
-    #'working_dir': '/home/box/web/',
+    'working_dir': '/home/box/web/',
     # 'python': '/usr/bin/python',
     'args': (
-        '--chdir=/home/box/web',
+        
         '--bind=0.0.0.0:8000',
         '--workers=16',
         '--timeout=60',
