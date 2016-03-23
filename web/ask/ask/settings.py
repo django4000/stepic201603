5298a43ae63649ea77c2b8b1f75af6cb1a5eb917
sasha <sasha@localhost.localdomain>
2016-03-23 22:49:42 +0300

    mysql

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'NAME': 'db',
+	'USER': 'root',
+	'OPTIONS':{
+	default-character-set= utf8,
+	
+	}
     }
 }
 
